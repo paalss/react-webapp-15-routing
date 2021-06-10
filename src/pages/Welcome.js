743e080d663d2967ab8904e1f9a6c5ab542ecf31
Pål Stakvik <paalss@Pals-MacBook-Pro.local>
2021-06-10 14:06:41 +0200

    nested routing

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import { Route } from "react-router"
+
+
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <Route path="/welcome/new-user">
+        <p>Hellooooo</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
